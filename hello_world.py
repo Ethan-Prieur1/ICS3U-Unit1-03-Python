ab6f4275bf533668619fb5282490f3c571eacf72
Ethan Prieur <ethan.prieur@mths.ca>
2022-02-17 19:50:52 +0000

    added Hello, World!

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Ethan Prieur
+# Created on February 17th 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
